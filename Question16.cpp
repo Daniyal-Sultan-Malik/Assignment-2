644c550e1a63df72b8aadbb79eb31e9d0aaf2095
Daniyal-Sultan-Malik <113673899+Daniyal-Sultan-Malik@users.noreply.github.com>
2022-10-03 20:24:23 +0500

    Update Question16.cpp

--- Question16.cpp
+++ Question16.cpp
@@ -4,11 +4,10 @@
 //
 //int main()
 //{
-//	int N, S, FirstPow = 0, SecondPow=1, i = 1, sum = 0, CurrentBase, CurrentPow;
+//	int N, FirstPow = 0, SecondPow=1, i = 1, sum = 0, CurrentBase, CurrentPow;
+//  const int S = 1;
 //	cout << "Enter Terms to be output 'N': ";
 //	cin >> N;
-//	cout << "Enter Value of Exponent 'S' : ";
-//	cin >> S;
 //	sum = pow(1, S + 1);
 //	cout << "1^(" << S + 1 << ")";
 //	while (i <= N-1) {
