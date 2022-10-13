740f339debb866ac62625f2f5a50933bc9bae14a
Daniyal-Sultan-Malik <113673899+Daniyal-Sultan-Malik@users.noreply.github.com>
2022-10-13 12:11:30 +0500

    Update Question14.cpp

--- Question14.cpp
+++ Question14.cpp
@@ -7,13 +7,16 @@
 //	bool i_1Prime=false, i_2Prime=false;
 //	cout << "Enter number to iterate up to: ";
 //	cin >> N;
+//	if (N > 3 && N % 2 == 0 && N % 3 == 0) {
+//		cout << "\nDual Primes: \n(2 , 3)";
+//	}
 //	while (i <= N) {
 //		i_1 = i;
 //		i_2 = i - 2;
 //		if (i_1 % i_1 == 0 && i_1 % 1 == 0) {
 //			i_1Prime = true;
 //		}
-//		while (c < i_1/2) {
+//		while (c <= i_1/2) {
 //			if (i_1== 2 || i_1 == 3 || i_1 == 5 || i_1 == 7) {
 //				break;
 //			}
@@ -27,7 +30,7 @@
 //		if (i_2 % i_2 == 0 && i_2 % 1 == 0) {
 //			i_2Prime = true;
 //		}
-//		while (c < i_2/2) {
+//		while (c <= i_2/2) {
 //			if (i_2 == 2 || i_2 == 3 || i_2 == 5 || i_2 == 7) {
 //				break;
 //			}
